a0ae93d4a5b24583f2dc7ece81ecd4026593cd75
msavioti <angelosavioti@gmail.com>
2022-08-16 04:13:43 -0300

    perf: improved key search performance

--- lib/modules/generate_hashtags/data/datasources/generate_hashtags_datasource_impl.dart
+++ lib/modules/generate_hashtags/data/datasources/generate_hashtags_datasource_impl.dart
@@ -21,49 +21,22 @@ class GenerateHashtagsLocalDatasourceImpl
   Future<List<HashtagDataModel>> retrieveHashtagsForInput({
     required String input,
   }) async {
-    final noPunctutationInput = input.replaceAll(RegExp(r'[.,():?!]'), '');
-    final capitalWords = _filterCapitalWords(noPunctutationInput);
-    final hashtags = _detectKeyWords(capitalWords);
+    final hashtags = <HashtagDataModel>[];
+
+    for (var key in indexedKeys) {
+      if (input.contains(key)) {
+        final model = HashtagDataModel.fromMap(
+            database.firstWhere((e) => e['term'] == key));
+
+        if (!hashtags.contains(model)) {
+          hashtags.add(model);
+        }
+      }
+    }
     return hashtags;
   }
 
   void _indexKeys() {
     indexedKeys = database.map((e) => e['term'] as String).toList();
-  }
-
-  List<String> _filterCapitalWords(String input) {
-    final capitalWords =
-        input.split(' ').where((e) => e[0] == e[0].toUpperCase()).toList();
-    return capitalWords;
-  }
-
-  List<HashtagDataModel> _detectKeyWords(List<String> words) {
-    final hashtags = <HashtagDataModel>[];
-
-    for (var i = 0; i < words.length; i++) {
-      if (indexedKeys.contains(words[i])) {
-        final model = HashtagDataModel.fromMap(
-            database.firstWhere((e) => e['term'] == words[i]));
-        if (!hashtags.contains(model)) {
-          hashtags.add(model);
-        }
-        continue;
-      }
-
-      if (i + 1 < words.length) {
-        final key = '${words[i]} ${words[i + 1]}';
-
-        if (indexedKeys.contains(key)) {
-          final model = HashtagDataModel.fromMap(
-              database.firstWhere((e) => e['term'] == key));
-
-          if (!hashtags.contains(model)) {
-            hashtags.add(model);
-          }
-        }
-      }
-    }
-
-    return hashtags;
   }
 }
